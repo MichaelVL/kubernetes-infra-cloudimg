3309c8f61d387be1f8b52d881202eefa4d0b85df
MichaelVL <mvl.gh@network42.dk>
2018-01-07 17:33:51 +0100

    Remove ingress resource manifests, add kube-lego image

--- scripts/kubernetes_ingress.sh
+++ scripts/kubernetes_ingress.sh
@@ -1,18 +1,10 @@
 #!/bin/bash -eux
 
-# https://github.com/kubernetes/ingress-nginx/blob/master/deploy/README.md
-echo "--> Pulling Ingress controller manifests"
-mkdir -p /etc/kubernetes/addon-manifests/ingress
-cd /etc/kubernetes/addon-manifests/ingress
-curl -sO https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/namespace.yaml
-curl -sO https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/default-backend.yaml
-curl -sO https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/configmap.yaml
-curl -sO https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/tcp-services-configmap.yaml
-curl -sO https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/udp-services-configmap.yaml
-curl -sO https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/rbac.yaml
-curl -sO https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/with-rbac.yaml
-curl -sO https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/baremetal/service-nodeport.yaml
+# https://github.com/kubernetes/charts/tree/master/stable/nginx-ingress
 
-echo "--> Pulling Ingress controller images"
+docker pull gcr.io/google_containers/nginx-ingress-controller:0.9.0-beta.15
 docker pull gcr.io/google_containers/defaultbackend:1.4
-docker pull quay.io/kubernetes-ingress-controller/nginx-ingress-controller:0.9.0
+
+# https://github.com/kubernetes/charts/tree/master/stable/kube-lego
+
+docker pull jetstack/kube-lego:0.1.5
