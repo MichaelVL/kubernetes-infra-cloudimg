579108b30b78ad427e558d793be08e4b0f67e031
MichaelVL <mvl.gh@network42.dk>
2018-02-12 18:03:08 +0100

    Update Prometheus and Grafana versions

--- scripts/kubernetes_prometheus_grafana.sh
+++ scripts/kubernetes_prometheus_grafana.sh
@@ -5,11 +5,12 @@ echo "--> Fetching Prometheus and Grafana images"
 # https://github.com/kubernetes/charts/tree/master/stable/prometheus
 docker pull prom/alertmanager:v0.9.1
 docker pull jimmidyson/configmap-reload:v0.1
-docker pull gcr.io/google_containers/kube-state-metrics:v1.1.0-rc.0
+docker pull gcr.io/google_containers/kube-state-metrics:v1.1.0
 docker pull prom/node-exporter:v0.15.2
-docker pull prom/prometheus:v1.8.2
+docker pull prom/prometheus:v2.1.0
 docker pull prom/pushgateway:v0.4.0
+docker pull appropriate/curl:latest
 
 # https://github.com/kubernetes/charts/tree/master/stable/grafana
 # https://hub.docker.com/r/grafana/grafana/tags/
-docker pull grafana/grafana:4.6.2
+docker pull grafana/grafana:4.6.3
