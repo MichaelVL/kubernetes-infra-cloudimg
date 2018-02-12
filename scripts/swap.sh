5fee36287b04f6cf8db9d77c451fe54feac8dbab
MichaelVL <mvl.gh@network42.dk>
2018-02-12 18:02:06 +0100

    Remove swap partition

--- scripts/swap.sh
+++ scripts/swap.sh
@@ -1,5 +1,22 @@
 #!/bin/bash -eux
 
+swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
+swappart=$(readlink -f /dev/disk/by-uuid/$swapuuid)
+echo "  Swap partition: $swappart UUID=$swapuuid"
+/sbin/swapoff "${swappart}"
+
 echo "--> Remove swap"
 sed -i '/ swap / s/^/#/' /etc/fstab
+echo "--> Content of /etc/fstab"
 cat /etc/fstab
+
+fdisk /dev/vda <<EOF
+d
+5
+d
+2
+w
+EOF
+
+echo "--> Partitions"
+fdisk -l /dev/vda
