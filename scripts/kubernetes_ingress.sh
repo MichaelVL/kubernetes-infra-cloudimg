0d913fbec257138eae3cca140850bfa804319179
MichaelVL <mvl.gh@network42.dk>
2018-02-14 18:58:00 +0100

    Change to cert-manager from kube-lego

--- scripts/kubernetes_ingress.sh
+++ scripts/kubernetes_ingress.sh
@@ -5,6 +5,6 @@
 docker pull gcr.io/google_containers/nginx-ingress-controller:0.9.0-beta.15
 docker pull gcr.io/google_containers/defaultbackend:1.4
 
-# https://github.com/kubernetes/charts/tree/master/stable/kube-lego
-
-docker pull jetstack/kube-lego:0.1.5
+# Cert manager deprecates kube-lego
+docker pull quay.io/jetstack/cert-manager-controller:v0.2.3
+docker pull quay.io/jetstack/cert-manager-ingress-shim:v0.2.3
