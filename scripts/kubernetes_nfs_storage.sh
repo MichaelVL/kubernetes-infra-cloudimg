b36dcd2a9458605035d6bad3d1fa0d25b9a5202e
MichaelVL <mvl.gh@network42.dk>
2018-08-14 15:17:33 +0200

    Updates for Kubernetes 1.11.2

--- scripts/kubernetes_nfs_storage.sh
+++ scripts/kubernetes_nfs_storage.sh
@@ -10,22 +10,15 @@ KUBE_MM="$KUBE_MAJOR.$KUBE_MINOR"
 mkdir /srv
 
 # https://github.com/kubernetes-incubator/external-storage/tree/master/nfs
-echo "--> Pulling NFS storage manifests"
+echo "--> Fetching NFS storage manifests"
 mkdir -p /etc/kubernetes/addon-manifests/nfs-storage-provisioner
 cd /etc/kubernetes/addon-manifests/nfs-storage-provisioner
-curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/auth/deployment-sa.yaml
-curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/auth/daemonsett-sa.yaml
-curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/class.yaml
-curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/auth/serviceaccount.yaml
-curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/auth/psp.yaml
-curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/auth/clusterrole.yaml
-curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/auth/clusterrolebinding.yaml
 
-if [ "$KUBE_MM" == "1.9" ] ; then
-  # Moved to 'v1' in 1.8
-  sed -i -e 's/apiVersion: rbac.authorization.k8s.io\/v1alpha1/apiVersion: rbac.authorization.k8s.io\/v1/' clusterrole.yaml
-  sed -i -e 's/apiVersion: rbac.authorization.k8s.io\/v1alpha1/apiVersion: rbac.authorization.k8s.io\/v1/' clusterrolebinding.yaml
-fi
+curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/psp.yaml
+curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/rbac.yaml
+curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/deployment.yaml
+curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/statefulset.yaml
+curl -O https://raw.githubusercontent.com/kubernetes-incubator/external-storage/master/nfs/deploy/kubernetes/class.yaml
 
 echo "--> Pulling NFS storage image"
-docker pull quay.io/kubernetes_incubator/nfs-provisioner:v1.0.9
+docker pull quay.io/kubernetes_incubator/nfs-provisioner:latest
