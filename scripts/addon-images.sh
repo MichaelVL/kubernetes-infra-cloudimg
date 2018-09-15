1b695648a288d9feaaa5755589cba203e26206f2
MichaelVL <mvl.gh@network42.dk>
2018-09-15 15:45:13 +0200

    Editorial change

--- scripts/addon-images.sh
+++ scripts/addon-images.sh
@@ -8,7 +8,6 @@ docker pull python:2-slim
 docker pull nginx:1.11-alpine
 docker pull bitnami/rabbitmq:3.7.2-r1
 docker pull bitnami/mongodb:3.7.1-r0
-#docker pull python:2-slim
 
 echo "--> Images available after installing additional images"
 docker images
