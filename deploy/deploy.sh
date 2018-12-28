38c0b7ad0a11f39a3edbd9aad84fc1b029a6ed01
MichaelVL <mvl.gh@network42.dk>
2018-12-28 19:20:02 +0100

    Correct rook/ceph storage dir. Add non-Helm deployment script

---
+++ deploy/deploy.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# This script deploys non-Helm components
+
+set -ex
+
+function deploy_flannel {
+    export KUBECONFIG=/etc/kubernetes/admin.conf
+    kubectl apply -f /etc/kubernetes/addon-manifests/flannel/kube-flannel.yml
+}
+
+function deploy_canal {
+    export KUBECONFIG=/etc/kubernetes/admin.conf
+    kubectl apply -f /etc/kubernetes/addon-manifests/canal/rbac.yaml
+    kubectl apply -f /etc/kubernetes/addon-manifests/canal/canal.yaml
+}
+
+function deploy_calico {
+    export KUBECONFIG=/etc/kubernetes/admin.conf
+    cp /etc/kubernetes/addon-manifests/calico/calico.yaml /etc/kubernetes/addon-manifests/calico/calico-orig.yaml
+    sed -i -e 's/192.168.0.0/10.244.0.0/' /etc/kubernetes/addon-manifests/calico/calico.yaml
+    kubectl apply -f /etc/kubernetes/addon-manifests/calico/rbac-kdd.yaml
+    kubectl apply -f /etc/kubernetes/addon-manifests/calico/calico.yaml
+}
+
+function deploy_weave {
+    export KUBECONFIG=/etc/kubernetes/admin.conf
+    kubectl apply -n kube-system -f /etc/kubernetes/addon-manifests/weave-net
+}
+
+function deploy_dashboard {
+    export KUBECONFIG=/etc/kubernetes/admin.conf
+    kubectl apply -n kube-system -f /etc/kubernetes/addon-manifests/dashboard
+}
+
+function deploy_nfs_storage_provider {
+    export KUBECONFIG=/etc/kubernetes/admin.conf
+    export NAMESPACE=default
+    #export NAMESPACE=nfs-storage
+    #kubectl create ns \$NAMESPACE
+    kubectl create -n  \$NAMESPACE -f /etc/kubernetes/addon-manifests/nfs-storage-provisioner/rbac.yaml
+    kubectl create -n  \$NAMESPACE -f /etc/kubernetes/addon-manifests/nfs-storage-provisioner/psp.yaml
+    kubectl create -n  \$NAMESPACE -f /etc/kubernetes/addon-manifests/nfs-storage-provisioner/deployment.yaml
+    kubectl create -n  \$NAMESPACE -f /etc/kubernetes/addon-manifests/nfs-storage-provisioner/class.yaml
+    kubectl annotate -n \$NAMESPACE storageclass example-nfs storageclass.beta.kubernetes.io/is-default-class=true
+}
+
+function deploy_rook_ceph_storage_provider {
+    export KUBECONFIG=/etc/kubernetes/admin.conf
+    kubectl create -f /etc/kubernetes/addon-manifests/rook-ceph-storage-provisioner/operator.yaml
+    kubectl create -f /etc/kubernetes/addon-manifests/rook-ceph-storage-provisioner/cluster.yaml
+    kubectl create -f /etc/kubernetes/addon-manifests/rook-ceph-storage-provisioner/storageclass.yaml
+    kubectl annotate -n \$NAMESPACE storageclass rook-ceph-block storageclass.beta.kubernetes.io/is-default-class=true
+}
+
+function deploy_metrics_server {
+    export KUBECONFIG=/etc/kubernetes/admin.conf
+    kubectl create -f /etc/kubernetes/addon-manifests/metrics-server
+}
+
+while [[ $# -gt 0 ]]
+do
+  key="$1"
+  case $key in
+    --flannel)
+	deploy_flannel
+	;;
+    --canall)
+	deploy_canal
+	;;
+    --calico)
+	deploy_calico
+	;;
+    --weave)
+	deploy_weave
+	;;
+    --nfs-provisioner)
+	deploy_nfs_storage_provider
+	;;
+    --rook-ceph-provisioner)
+	deploy_rook_ceph_storage_provider
+	;;
+    --metrics-server)
+	deploy_metrics_server
+	;;
+  esac
+  shift
+done
--- scripts/kubernetes_rook_ceph.sh
+++ scripts/kubernetes_rook_ceph.sh
@@ -2,12 +2,13 @@
 
 set -eux
 
+CEPH_VER="v13.2"
+
 apt-get install -y xfsprogs
 
-# See deployment.yaml - uses hostPath
-mkdir -p /srv
+mkdir -p /rook/storage-dir
 
-# https://github.com/kubernetes-incubator/external-storage/tree/master/nfs
+# https://github.com/rook/rook/tree/master/cluster/examples/kubernetes/ceph
 echo "--> Fetching ROOK/Ceph storage manifests"
 mkdir -p /etc/kubernetes/addon-manifests/rook-ceph-storage-provisioner
 cd /etc/kubernetes/addon-manifests/rook-ceph-storage-provisioner
@@ -16,7 +17,10 @@ curl -O https://raw.githubusercontent.com/rook/rook/master/cluster/examples/kube
 curl -O https://raw.githubusercontent.com/rook/rook/master/cluster/examples/kubernetes/ceph/cluster.yaml
 curl -O https://raw.githubusercontent.com/rook/rook/master/cluster/examples/kubernetes/ceph/storageclass.yaml
 
-sed -i -e 's|dataDirHostPath: /var/lib/rook|dataDirHostPath: /srv|' cluster.yaml
+sed -i -e "s|    image: ceph/ceph:v13|    image: ceph/ceph:$CEPH_VER|" cluster.yaml
+sed -i -e 's|#    directories:|    directories:|' cluster.yaml
+sed -i -e 's|#    - path: /rook/storage-dir|    - path: /rook/storage-dir|' cluster.yaml
 
-echo "--> Pulling ROOK/Ceph image"
+echo "--> Pulling ROOK/Ceph images"
 docker pull rook/ceph:master
+docker pull ceph/ceph:$CEPH_VER
