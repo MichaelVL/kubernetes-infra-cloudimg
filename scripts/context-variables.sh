d2264e16e2b2060fecbb6ebe5689be2127195b7a
MichaelVL <mvl.gh@network42.dk>
2019-03-05 19:49:01 +0100

    Update Prometheus, Grafana and Traefik

--- scripts/context-variables.sh
+++ scripts/context-variables.sh
@@ -1,13 +1,13 @@
 export HELM_VERSION='v2.13.0'
 
 export HELM_PROMETHEUS_CHART='stable/prometheus'
-export HELM_PROMETHEUS_VERSION='8.7.1'
+export HELM_PROMETHEUS_VERSION='8.8.1'
 
 export HELM_GRAFANA_CHART='stable/grafana'
-export HELM_GRAFANA_VERSION='2.2.0'
+export HELM_GRAFANA_VERSION='2.2.1'
 
 export HELM_TRAEFIK_CHART='stable/traefik'
-export HELM_TRAEFIK_VERSION='1.61.1'
+export HELM_TRAEFIK_VERSION='1.63.0'
 
 export HELM_METALLB_CHART='stable/metallb'
 export HELM_METALLB_VERSION='0.8.4'
