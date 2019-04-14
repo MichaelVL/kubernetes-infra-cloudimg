b7125cbe87ed0dcf564fe12421c9faa41446b6f3
MichaelVL <mvl.gh@network42.dk>
2019-04-14 19:06:30 +0200

    Remove unused/redundant components

--- scripts/cert_manager.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-echo "--> Fetching cert-manager manifests"
-
-mkdir -p /etc/kubernetes/addon-manifests/cert-manager
-cd /etc/kubernetes/addon-manifests/cert-manager
-curl -sO https://raw.githubusercontent.com/jetstack/cert-manager/release-0.6/deploy/manifests/00-crds.yaml
