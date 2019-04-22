96eb585caaed7efe8f728a73c10b5e9604f19518
MichaelVL <mvl.gh@network42.dk>
2019-04-22 16:36:58 +0200

    Component updates

--- scripts/context-variables.sh
+++ scripts/context-variables.sh
@@ -1,13 +1,13 @@
-export HELM_VERSION='v2.13.0'
+export HELM_VERSION='v2.13.1'
 
 export HELM_PROMETHEUS_CHART='stable/prometheus'
-export HELM_PROMETHEUS_VERSION='8.9.2'
+export HELM_PROMETHEUS_VERSION='8.10.1'
 
 export HELM_GRAFANA_CHART='stable/grafana'
-export HELM_GRAFANA_VERSION='3.3.0'
+export HELM_GRAFANA_VERSION='3.3.1'
 
 export HELM_METALLB_CHART='stable/metallb'
-export HELM_METALLB_VERSION='0.9.4'
+export HELM_METALLB_VERSION='0.9.5'
 
 export HELM_METRICS_SERVER_CHART='stable/metrics-server'
 export HELM_METRICS_SERVER_VERSION='2.6.0'
