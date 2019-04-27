ec411fdc9fa356b77d865f97612d72464493614a
MichaelVL <mvl.gh@network42.dk>
2019-04-27 08:48:10 +0200

    Update componets

--- scripts/context-variables.sh
+++ scripts/context-variables.sh
@@ -1,20 +1,20 @@
 export HELM_VERSION='v2.13.1'
 
 export HELM_PROMETHEUS_CHART='stable/prometheus'
-export HELM_PROMETHEUS_VERSION='8.10.1'
+export HELM_PROMETHEUS_VERSION='8.10.3'
 
 export HELM_GRAFANA_CHART='stable/grafana'
-export HELM_GRAFANA_VERSION='3.3.1'
+export HELM_GRAFANA_VERSION='3.3.2'
 
 export HELM_METALLB_CHART='stable/metallb'
 export HELM_METALLB_VERSION='0.9.5'
 
 export HELM_METRICS_SERVER_CHART='stable/metrics-server'
-export HELM_METRICS_SERVER_VERSION='2.6.0'
+export HELM_METRICS_SERVER_VERSION='2.7.0'
 
 export HELM_CERT_MANAGER_CHART='stable/cert-manager'
 export HELM_CERT_MANAGER_VERSION='v0.6.7'
 
 export HELM_CONTOUR_CHART='contour'
-export HELM_CONTOUR_VERSION='0.2.1'
+export HELM_CONTOUR_VERSION='0.2.3'
 export HELM_CONTOUR_URL='https://raw.githubusercontent.com/MichaelVL/contour-envoy-helm-chart/master/'
