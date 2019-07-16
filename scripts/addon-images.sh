aaf2ca09e8aac0632bac0a3e06e76c230ac955cc
MichaelVL <mvl.gh@network42.dk>
2019-07-16 19:40:28 +0200

    Remove version on Nginx image

--- scripts/addon-images.sh
+++ scripts/addon-images.sh
@@ -5,7 +5,7 @@ set -eux
 echo "--> Pulling a selection of Docker base images"
 crictl pull busybox
 crictl pull python:2-slim
-crictl pull nginx:1.11-alpine
+crictl pull nginx
 crictl pull bitnami/rabbitmq:3.7.2-r1
 crictl pull bitnami/mongodb:3.7.1-r0
 
