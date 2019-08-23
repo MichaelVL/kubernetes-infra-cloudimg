b5c6a28f854499594037356b9dd12b4c6c130a4b
MichaelVL <mvl.gh@network42.dk>
2019-08-23 16:22:10 +0200

    Fix vpa image pull

--- scripts/kubernetes_vpa.sh
+++ scripts/kubernetes_vpa.sh
@@ -9,14 +9,14 @@ cd /etc/kubernetes/addon-manifests/vpa
 git clone https://github.com/kubernetes/autoscaler.git
 rm -rf addon-resizer cluster-autoscaler builder
 
-IMAGE=$(grep 'image\:' autoscaler/vertical-pod-autoscaler/deploy/admission-controller-deployment.yaml | cut -d':' -f3)
+IMAGE=$(grep 'image\:' autoscaler/vertical-pod-autoscaler/deploy/admission-controller-deployment.yaml | cut -d':' -f2,3)
 echo "--> Pulling VPA controller image ($IMAGE)"
 crictl pull $IMAGE
 
-IMAGE=$(grep 'image\:' autoscaler/vertical-pod-autoscaler/deploy/recommender-deployment.yaml | cut -d':' -f3)
+IMAGE=$(grep 'image\:' autoscaler/vertical-pod-autoscaler/deploy/recommender-deployment.yaml | cut -d':' -f2,3)
 echo "--> Pulling VPA recommender image ($IMAGE)"
 crictl pull $IMAGE
 
-IMAGE=$(grep 'image\:' autoscaler/vertical-pod-autoscaler/deploy/updater-deployment.yaml | cut -d':' -f3)
+IMAGE=$(grep 'image\:' autoscaler/vertical-pod-autoscaler/deploy/updater-deployment.yaml | cut -d':' -f2,3)
 echo "--> Pulling VPA updater image ($IMAGE)"
 crictl pull $IMAGE
