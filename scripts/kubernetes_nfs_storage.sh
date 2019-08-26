0ec061c67c2065f3ab12ce04eff3c3229fc102eb
MichaelVL <mvl.gh@network42.dk>
2019-08-26 19:26:11 +0200

    Clarify need for NFS package

--- scripts/kubernetes_nfs_storage.sh
+++ scripts/kubernetes_nfs_storage.sh
@@ -2,6 +2,9 @@
 
 set -eux
 
+# The NFS storage provisioner does not require additional NFS functionality, but
+# the dynamically provisioned PVs are of type nfs and thus the nodes mounting
+#these drives need NFS functionality
 apt-get install -y nfs-common
 
 # See deployment.yaml - uses hostPath
