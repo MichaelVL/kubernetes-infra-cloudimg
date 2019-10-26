1136f008dc13582d4579d61aacc6bb260e738d28
MichaelVL <mvl.gh@network42.dk>
2019-10-26 19:43:28 +0200

    Switch to helm2yaml for getting pre-pull image list

--- scripts/get_helm_image_list.sh
+++ scripts/get_helm_image_list.sh
@@ -2,52 +2,16 @@
 
 set -eux
 
-source scripts/context-variables.sh
+HELM_VERSION='v2.15.1'
+
+HELM2YAML='michaelvl/helm2yaml:latest'
+APIVERSIONS='--api-versions apiregistration.k8s.io/v1beta1 --api-versions apiextensions.k8s.io/v1beta1'
+CHARTS='deploy/prometheus.yaml deploy/prometheus-adapter.yaml deploy/grafana.yaml deploy/loki.yaml deploy/metallb.yaml deploy/metrics-server.yaml deploy/cert-manager.yaml deploy/nfs-storage-provisioner.yaml'
 
 echo "$1 gcr.io/kubernetes-helm/tiller:$HELM_VERSION"
 
-echo "# ${HELM_PROMETHEUS_CHART} ${HELM_PROMETHEUS_VERSION}"
-HELM_VALUES=$(helm inspect values ${HELM_PROMETHEUS_CHART} --version ${HELM_PROMETHEUS_VERSION} |yq .)
-for component in alertmanager configmapReload kubeStateMetrics nodeExporter server pushgateway; do
-    IMAGE=`echo ${HELM_VALUES}|jq -r ".${component}.image | [.repository, .tag] | join(\":\")"`
-    echo "$1 ${IMAGE}"
+for chart in $CHARTS; do
+    for img in $(docker run --rm -v $(pwd):/src:ro $HELM2YAML $APIVERSIONS --list-images helmsman -f $chart); do
+        echo "$1 $img"
+    done
 done
-
-echo "# ${HELM_GRAFANA_CHART} ${HELM_GRAFANA_VERSION}"
-HELM_VALUES=$(helm inspect values ${HELM_GRAFANA_CHART} --version ${HELM_GRAFANA_VERSION} |yq .)
-for component in image downloadDashboardsImage; do
-    IMAGE=`echo ${HELM_VALUES}|jq -r ".${component} | [.repository, .tag] | join(\":\")"`
-    echo "$1 ${IMAGE}"
-done
-IMAGE=`echo ${HELM_VALUES}|jq -r ".sidecar.image"`
-echo "$1 ${IMAGE}"
-
-echo "# ${HELM_METALLB_CHART} ${HELM_METALLB_VERSION}"
-HELM_VALUES=$(helm inspect values ${HELM_METALLB_CHART} --version ${HELM_METALLB_VERSION} |yq .)
-for component in controller speaker; do
-    IMAGE=`echo ${HELM_VALUES}|jq -r ".${component}.image | [.repository, .tag] | join(\":\")"`
-    echo "$1 ${IMAGE}"
-done
-
-echo "# ${HELM_METRICS_SERVER_CHART} ${HELM_METRICS_SERVER_VERSION}"
-HELM_VALUES=$(helm inspect values ${HELM_METRICS_SERVER_CHART} --version ${HELM_METRICS_SERVER_VERSION} |yq .)
-IMAGE=`echo ${HELM_VALUES}|jq -r "[.image.repository, .image.tag] | join(\":\")"`
-echo "$1 ${IMAGE}"
-
-echo "# ${HELM_CERT_MANAGER_CHART} ${HELM_CERT_MANAGER_VERSION}"
-HELM_VALUES=$(helm inspect values ${HELM_CERT_MANAGER_CHART} --version ${HELM_CERT_MANAGER_VERSION} |yq .)
-#IMAGE=`echo ${HELM_VALUES}|jq -r "[.image.repository, .image.tag] | join(\":\")"`
-IMAGE=`echo ${HELM_VALUES}|jq -r ".image.repository"`
-echo "$1 ${IMAGE}:${HELM_CERT_MANAGER_VERSION}"
-
-echo "# ${HELM_CONTOUR_CHART} ${HELM_CONTOUR_VERSION} ${HELM_CONTOUR_URL}"
-HELM_VALUES=$(helm inspect values ${HELM_CONTOUR_CHART} --repo ${HELM_CONTOUR_URL} --version ${HELM_CONTOUR_VERSION} |yq .)
-for component in contour envoy; do
-    IMAGE=`echo ${HELM_VALUES}|jq -r ".${component}.image | [.repository, .tag] | join(\":\")"`
-    echo "$1 ${IMAGE}"
-done
-
-echo "# ${HELM_NFS_STORAGE_PROVISIONER_CHART} ${HELM_NFS_STORAGE_PROVISIONER_VERSION}"
-HELM_VALUES=$(helm inspect values ${HELM_NFS_STORAGE_PROVISIONER_CHART} --version ${HELM_NFS_STORAGE_PROVISIONER_VERSION} |yq .)
-IMAGE=`echo ${HELM_VALUES}|jq -r "[.image.repository, .image.tag] | join(\":\")"`
-echo "$1 ${IMAGE}"
