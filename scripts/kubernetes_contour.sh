506e33b60acc77d4dd9608a2c9aef1a1e67c2830
MichaelVL <mvl.gh@network42.dk>
2019-11-23 18:29:05 +0100

    Update to Contour 1.0.0

--- scripts/kubernetes_contour.sh
+++ scripts/kubernetes_contour.sh
@@ -5,18 +5,19 @@ set -eux
 echo "--> Pulling Contour"
 mkdir -p /etc/kubernetes/addon-manifests/contour
 cd /etc/kubernetes/addon-manifests/contour
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/v1.0.0-beta.1/examples/contour/01-common.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/v1.0.0-beta.1/examples/contour/01-contour-config.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/v1.0.0-beta.1/examples/contour/02-job-certgen.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/v1.0.0-beta.1/examples/contour/02-rbac.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/v1.0.0-beta.1/examples/contour/02-service-contour.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/v1.0.0-beta.1/examples/contour/02-service-envoy.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/v1.0.0-beta.1/examples/contour/03-contour.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/v1.0.0-beta.1/examples/contour/03-envoy.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/00-common.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/01-contour-config.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/01-crds.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/02-job-certgen.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/02-rbac.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/02-service-contour.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/02-service-envoy.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/03-contour.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/03-envoy.yaml
 
 # Dashboard
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/v1.0.0-beta.1/examples/grafana/01-namespace.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/v1.0.0-beta.1/examples/grafana/02-grafana-configmap.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/grafana/01-namespace.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/grafana/02-grafana-configmap.yaml
 
-crictl pull docker.io/projectcontour/contour:v1.0.0-beta.1
-crictl pull docker.io/envoyproxy/envoy:v1.11.1
+crictl pull docker.io/projectcontour/contour:v1.0.0
+crictl pull docker.io/envoyproxy/envoy:v1.11.2
