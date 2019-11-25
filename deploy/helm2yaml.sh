623608e3707b5aeaa36c29accb62e6478442fce8
MichaelVL <mvl.gh@network42.dk>
2019-11-25 21:43:42 +0100

    Refactor chart rendering and deployment

--- deploy/helm2yaml.sh
+++ deploy/helm2yaml.sh
@@ -13,6 +13,6 @@ mkdir -p "$render_to"
 
 # Map current folder as /src and assume all files passed with -f are relative to this
 # Env HOME setting is necessary because Helm3 use HOME for config files
-docker run --rm --user $(id -u):$(id -g) -e HOME=/tmp/home -v $(pwd):/src:ro -v $(pwd)/${render_to}:/rendered:rw $HELM2YAML_IMAGE --api-versions apiregistration.k8s.io/v1beta1 --api-versions apiextensions.k8s.io/v1beta1 --auto-api-upgrade --render-to /rendered/${appname}.yaml --render-namespace-to /rendered/${appname}-ns.yaml --hook-filter '' helmsman -f ${filename}
+docker run --rm --user $(id -u):$(id -g) -e HOME=/tmp/home -v $(pwd):/src:ro -v $(pwd)/${render_to}:/rendered:rw $HELM2YAML_IMAGE --api-versions apiregistration.k8s.io/v1beta1 --api-versions apiextensions.k8s.io/v1beta1 --auto-api-upgrade --render-to /rendered/${appname}.yaml --render-w-ns-to /rendered/${appname}-w-ns.yaml --render-namespace-to /rendered/${appname}-ns.yaml --hook-filter '' helmsman -f ${filename}
 
-./deploy/kubeaudit.sh all -f $render_to/${appname}.yaml -f $render_to/${appname}-ns.yaml 2> $render_to/${appname}-audit.txt
+./deploy/kubeaudit.sh all -f $render_to/${appname}.yaml -f $render_to/${appname}-w-ns.yaml -f $render_to/${appname}-ns.yaml 2> $render_to/${appname}-audit.txt
--- deploy/manifest_deploy.sh
+++ deploy/manifest_deploy.sh
@@ -3,18 +3,26 @@
 HELM2YAML_IMAGE='michaelvl/helm2yaml'
 KUBECTL_IMAGE='bitnami/kubectl:1.15.0'
 
-#set -xe
+set -xe
 
 manifest_path=$1
 namespace_path=$2
+manifest_w_explicit_ns_path=$3
 
 # Pass-in both the user .kube folder and the current value of KUBECONFIG
 KUBECFG="-v ${HOME}/.kube:${HOME}/.kube"
 MANIFESTS="-v $(pwd):/work"
 KUBECTL_CMD="docker run -i --user $(id -u):$(id -g) --rm -e KUBECONFIG $KUBECFG:ro -w /work $MANIFESTS:ro ${KUBECTL_IMAGE}"
 
-$KUBECTL_CMD apply -f $namespace_path
-NS=$(yq -r '.metadata.name' $namespace_path)
+if [ -f $manifest_path ]; then
+    $KUBECTL_CMD apply -f $namespace_path
+    NS=$(yq -r '.metadata.name' $namespace_path)
 
-# The environment variables shown here are usefull for deployment of e.g. https://github.com/MichaelVL/kubernetes-infra-cloudimg
-cat $manifest_path | docker run --rm -i --entrypoint /bin/k8envsubst.py -e GRAFANA_ADMIN_PASSWD -e DNS_DOMAIN -e NFS_STORAGE_PROVISIONER_HOSTNAME $HELM2YAML_IMAGE | $KUBECTL_CMD -n $NS apply -f -
+    # The environment variables shown here are usefull for deployment of e.g. https://github.com/MichaelVL/kubernetes-infra-cloudimg
+    cat $manifest_path | docker run --rm -i --entrypoint /bin/k8envsubst.py -e GRAFANA_ADMIN_PASSWD -e DNS_DOMAIN -e NFS_STORAGE_PROVISIONER_HOSTNAME $HELM2YAML_IMAGE | $KUBECTL_CMD -n $NS apply -f -
+fi
+
+if [ -f $manifest_w_explicit_ns_path ]; then
+    # The environment variables shown here are usefull for deployment of e.g. https://github.com/MichaelVL/kubernetes-infra-cloudimg
+    cat $manifest_path | docker run --rm -i --entrypoint /bin/k8envsubst.py -e GRAFANA_ADMIN_PASSWD -e DNS_DOMAIN -e NFS_STORAGE_PROVISIONER_HOSTNAME $HELM2YAML_IMAGE | $KUBECTL_CMD apply -f -
+fi
