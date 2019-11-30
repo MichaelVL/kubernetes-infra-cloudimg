13862869d384c8d82c3fe214ea8605b35727c651
MichaelVL <mvl.gh@network42.dk>
2019-11-30 17:50:09 +0100

    Check missing path

--- deploy/manifest_deploy.sh
+++ deploy/manifest_deploy.sh
@@ -14,7 +14,7 @@ KUBECFG="-v ${HOME}/.kube:${HOME}/.kube"
 MANIFESTS="-v $(pwd):/work"
 KUBECTL_CMD="docker run -i --user $(id -u):$(id -g) --rm -e KUBECONFIG $KUBECFG:ro -w /work $MANIFESTS:ro ${KUBECTL_IMAGE}"
 
-if [ -f $manifest_path ]; then
+if [ ! -z $manifest_path ] && [ -f $manifest_path ] && [ ! -z $namespace_path ] && [ -f $namespace_path ]; then
     $KUBECTL_CMD apply -f $namespace_path
     NS=$(yq -r '.metadata.name' $namespace_path)
 
@@ -22,7 +22,7 @@ if [ -f $manifest_path ]; then
     cat $manifest_path | docker run --rm -i --entrypoint /bin/k8envsubst.py -e GRAFANA_ADMIN_PASSWD -e DNS_DOMAIN -e NFS_STORAGE_PROVISIONER_HOSTNAME $HELM2YAML_IMAGE | $KUBECTL_CMD -n $NS apply -f -
 fi
 
-if [ -f $manifest_w_explicit_ns_path ]; then
+if [ ! -z $manifest_w_explicit_ns_path ] && [ -f $manifest_w_explicit_ns_path ]; then
     # The environment variables shown here are usefull for deployment of e.g. https://github.com/MichaelVL/kubernetes-infra-cloudimg
-    cat $manifest_path | docker run --rm -i --entrypoint /bin/k8envsubst.py -e GRAFANA_ADMIN_PASSWD -e DNS_DOMAIN -e NFS_STORAGE_PROVISIONER_HOSTNAME $HELM2YAML_IMAGE | $KUBECTL_CMD apply -f -
+    cat $manifest_w_explicit_ns_path | docker run --rm -i --entrypoint /bin/k8envsubst.py -e GRAFANA_ADMIN_PASSWD -e DNS_DOMAIN -e NFS_STORAGE_PROVISIONER_HOSTNAME $HELM2YAML_IMAGE | $KUBECTL_CMD apply -f -
 fi
