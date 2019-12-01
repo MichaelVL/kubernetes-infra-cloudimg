9fe0d9006a100d3cc80dc16884fda58ab88d77a7
MichaelVL <mvl.gh@network42.dk>
2019-12-01 18:45:55 +0100

    Update provisioner script. Update deploy targets to include namespaced resources

--- deploy/manifest_deploy.sh
+++ deploy/manifest_deploy.sh
@@ -14,12 +14,13 @@ KUBECFG="-v ${HOME}/.kube:${HOME}/.kube"
 MANIFESTS="-v $(pwd):/work"
 KUBECTL_CMD="docker run -i --user $(id -u):$(id -g) --rm -e KUBECONFIG $KUBECFG:ro -w /work $MANIFESTS:ro ${KUBECTL_IMAGE}"
 
-if [ ! -z $manifest_path ] && [ -f $manifest_path ] && [ ! -z $namespace_path ] && [ -f $namespace_path ]; then
+if [ ! -z $namespace_path ] && [ -f $namespace_path ]; then
     $KUBECTL_CMD apply -f $namespace_path
     NS=$(yq -r '.metadata.name' $namespace_path)
-
-    # The environment variables shown here are usefull for deployment of e.g. https://github.com/MichaelVL/kubernetes-infra-cloudimg
-    cat $manifest_path | docker run --rm -i --entrypoint /bin/k8envsubst.py -e GRAFANA_ADMIN_PASSWD -e DNS_DOMAIN -e NFS_STORAGE_PROVISIONER_HOSTNAME $HELM2YAML_IMAGE | $KUBECTL_CMD -n $NS apply -f -
+    if [ ! -z $manifest_path ] && [ -f $manifest_path ]; then
+        # The environment variables shown here are usefull for deployment of e.g. https://github.com/MichaelVL/kubernetes-infra-cloudimg
+        cat $manifest_path | docker run --rm -i --entrypoint /bin/k8envsubst.py -e GRAFANA_ADMIN_PASSWD -e DNS_DOMAIN -e NFS_STORAGE_PROVISIONER_HOSTNAME $HELM2YAML_IMAGE | $KUBECTL_CMD -n $NS apply -f -
+    fi
 fi
 
 if [ ! -z $manifest_w_explicit_ns_path ] && [ -f $manifest_w_explicit_ns_path ]; then
