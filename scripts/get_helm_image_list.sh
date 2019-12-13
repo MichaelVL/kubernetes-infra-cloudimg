3d95039dac1cb4a34ee6997ed78f0dab08166a02
MichaelVL <mvl.gh@network42.dk>
2019-12-13 16:18:40 +0100

    Remove API versions from image list creater

--- scripts/get_helm_image_list.sh
+++ scripts/get_helm_image_list.sh
@@ -3,11 +3,10 @@
 set -eux
 
 HELM2YAML='michaelvl/helm2yaml:latest'
-APIVERSIONS='--api-versions apiregistration.k8s.io/v1beta1 --api-versions apiextensions.k8s.io/v1beta1'
-CHARTS='deploy/prometheus.yaml deploy/prometheus-adapter.yaml deploy/grafana.yaml deploy/loki.yaml deploy/promtail.yaml deploy/metallb.yaml deploy/metrics-server.yaml deploy/cert-manager.yaml deploy/nfs-storage-provisioner.yaml'
+CHARTS='deploy/prometheus.yaml deploy/prometheus-adapter.yaml deploy/grafana.yaml deploy/loki.yaml deploy/promtail.yaml deploy/metallb.yaml deploy/metrics-server.yaml deploy/cert-manager.yaml deploy/nfs-server-storage-provisioner.yaml'
 
 for chart in $CHARTS; do
-    for img in $(docker run --rm -v $(pwd):/src:ro $HELM2YAML $APIVERSIONS --list-images helmsman -f $chart); do
+    for img in $(docker run --rm -v $(pwd):/src:ro $HELM2YAML --list-images helmsman -f $chart); do
         echo "$1 $img"
     done
 done
