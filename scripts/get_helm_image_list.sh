0336a2d32660e653b7c450b7258afe39e5e4d417
MichaelVL <mvl.gh@network42.dk>
2019-12-15 09:06:46 +0100

    Add sealed secret image pulling

--- scripts/get_helm_image_list.sh
+++ scripts/get_helm_image_list.sh
@@ -3,7 +3,7 @@
 set -eux
 
 HELM2YAML='michaelvl/helm2yaml:latest'
-CHARTS='deploy/prometheus.yaml deploy/prometheus-adapter.yaml deploy/grafana.yaml deploy/loki.yaml deploy/promtail.yaml deploy/metallb.yaml deploy/metrics-server.yaml deploy/cert-manager.yaml deploy/nfs-server-storage-provisioner.yaml'
+CHARTS='deploy/prometheus.yaml deploy/prometheus-adapter.yaml deploy/grafana.yaml deploy/loki.yaml deploy/promtail.yaml deploy/metallb.yaml deploy/metrics-server.yaml deploy/cert-manager.yaml deploy/nfs-server-storage-provisioner.yaml deploy/sealed-secrets.yaml'
 
 for chart in $CHARTS; do
     for img in $(docker run --rm -v $(pwd):/src:ro $HELM2YAML --list-images helmsman -f $chart); do
