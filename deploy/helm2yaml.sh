80dea1b1d1cc7f8b7ad6214a4988b84f6cb8d948
MichaelVL <mvl.gh@network42.dk>
2019-12-15 18:28:51 +0100

    Seal secrets as part of rendering

--- deploy/helm2yaml.sh
+++ deploy/helm2yaml.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
 HELM2YAML_IMAGE='michaelvl/helm2yaml'
+KUBESEAL_IMAGE='michaelvl/kubeseal'
 
-#set -xe
+#set -x
+set -e
 
 filename=$1
 render_to=$2
 env_vars=${3:-""}
+kubeseal_cert=sealed-secrets-pub-cert.pem
 
 basename=$(basename -- "$filename")
 appname="${basename%.*}"
@@ -32,3 +35,17 @@ docker run --rm --user $(id -u):$(id -g) -e HOME=/tmp/home $env_set \
        --hook-filter '' helmsman -f ${filename}
 
 ./deploy/kubeaudit.sh all -f $render_to/${appname}.yaml -f $render_to/${appname}-w-ns.yaml -f $render_to/${appname}-ns.yaml 2> $render_to/${appname}-audit.txt
+
+secrets_path=${render_to}/${appname}-secrets-ns.yaml
+secrets_w_explicit_ns_path=$render_to/${appname}-secrets-w-ns.yaml
+
+if [ ! -z $secrets_path ] && [ -f $secrets_path ]; then
+    cp $secrets_path "$secrets_path.bck"
+    cat "$secrets_path.bck" | docker run -i --rm --user $(id -u):$(id -g) -v $(pwd):/work:rw $KUBESEAL_IMAGE --cert $kubeseal_cert > $secrets_path
+    rm  "$secrets_path.bck"
+fi
+if [ ! -z $secrets_w_explicit_ns_path ] && [ -f $secrets_w_explicit_ns_path ]; then
+    cp $secrets_w_explicit_ns_path "$secrets_w_explicit_ns_path.bck"
+    cat "$secrets_w_explicit_ns_path.bck" | docker run -i --rm --user $(id -u):$(id -g) -v $(pwd):/work:rw $KUBESEAL_IMAGE --cert $kubeseal_cert > $secrets_w_explicit_ns_path
+    rm "$secrets_w_explicit_ns_path.bck"
+fi
