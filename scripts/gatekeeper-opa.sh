27ce11a6f03ad5a1a2d6ddfc5d01af68d97950f2
MichaelVL <mvl.gh@network42.dk>
2020-03-08 17:54:44 +0100

    Update components

--- scripts/gatekeeper-opa.sh
+++ scripts/gatekeeper-opa.sh
@@ -4,4 +4,4 @@ set -eux
 
 echo "--> Pulling Gatekeeper image"
 
-crictl pull quay.io/open-policy-agent/gatekeeper:v3.1.0-beta.5
+crictl pull quay.io/open-policy-agent/gatekeeper:v3.1.0-beta.7
--- scripts/kubernetes_contour.sh
+++ scripts/kubernetes_contour.sh
@@ -5,19 +5,21 @@ set -eux
 echo "--> Pulling Contour"
 mkdir -p /etc/kubernetes/addon-manifests/contour
 cd /etc/kubernetes/addon-manifests/contour
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/00-common.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/01-contour-config.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/01-crds.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/02-job-certgen.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/02-rbac.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/02-service-contour.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/02-service-envoy.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/03-contour.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/contour/03-envoy.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.2/examples/contour/00-common.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.2/examples/contour/01-contour-config.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.2/examples/contour/01-crds.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.2/examples/contour/02-job-certgen.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.2/examples/contour/02-rbac.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.2/examples/contour/02-service-contour.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.2/examples/contour/02-service-envoy.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.2/examples/contour/03-contour.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.2/examples/contour/03-envoy.yaml
 
 # Dashboard
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/grafana/01-namespace.yaml
-curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.0/examples/grafana/02-grafana-configmap.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.2/examples/grafana/01-namespace.yaml
+curl -sO https://raw.githubusercontent.com/projectcontour/contour/release-1.2/examples/grafana/02-grafana-configmap.yaml
 
-crictl pull docker.io/projectcontour/contour:v1.0.1
-crictl pull docker.io/envoyproxy/envoy:v1.12.2
+crictl pull docker.io/projectcontour/contour:v1.2.1
+# Bug in 1.2
+crictl pull docker.io/projectcontour/contour:latest
+crictl pull docker.io/envoyproxy/envoy:v1.13.1
